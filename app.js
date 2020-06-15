5fe1ade58c3de1955509c120008e4540b0514264
Blythe Pollard <blytheepollard@gmail.com>
2020-06-14 19:13:51 -0600

    completed 3.10 challenge

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const sumFunction = require('./utils.js') 
+
+const notes = require('./notes.js')
+
+console.log(notes)
+
+// const sumFunction = require('./utils.js') 
 
 //have to save the exports from utils here as a variable!
 
-const sum = sumFunction(1, 2)
+// const sum = sumFunction(1, 2)
 
-console.log(sum)
+// console.log(sum)
+
